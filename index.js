6d8db732e014db5de02563e5174d18a9ca9fdeb1
Joshua-Rhymer Bernardo <joshua.bernardo@live.com>
2021-07-06 22:20:54 +1000

    added switch statments and add and view functions

--- index.js
+++ index.js
@@ -1,47 +1,102 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const mysql = require('mysql');
 
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            message: 'Choose an option below',
-            name: 'options',
-            choices: [
-                "Add Employee",
-                "Add Department",
-                "Add role",
-                new inquirer.Separator(),
-                "View Employee",
-                "View Department",
-                "View role",
-                new inquirer.Separator(),
-                "View All",
-                new inquirer.Separator(),
-            ],
-            default: "View All",
-        },
-        // {
-        //     // type: 'password',
-        //     // message: 'What is your password?',
-        //     // name: 'password',
-        // },
-        // {
-        //     // type: 'password',
-        //     // message: 'Re-enter password to confirm:',
-        //     // name: 'confirm',
-        // },
-    ])
+const connection = mysql.createConnection({
+    host: 'localhost',
 
-    .then((response) => {
-        if (response.option === "Add Employee") {
-            addEmployee();
+    // Your port; if not 3306
+    port: 3306,
 
-        } else if (response.option === "Add Department") {
-            addDepartment();
+    // Your username
+    user: 'root',
 
-        }
-    })
+    // Be sure to update with your own MySQL password!
+    password: 'Root12345678',
+    database: 'CMS_db',
+});
+
+//----------------------------------------------------------------------------------------------------------------------------
+
+mainMenu();
+
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Choose an option below',
+                name: 'options',
+                choices: [
+                    "Add Employee",
+                    "Add Department",
+                    "Add role",
+                    new inquirer.Separator(),
+                    "View Employees",
+                    "View Departments",
+                    "View roles",
+                    new inquirer.Separator(),
+                    "View All",
+                    new inquirer.Separator(),
+                ],
+                default: "View All",
+            },
+            // {
+            //     // type: 'password',
+            //     // message: 'What is your password?',
+            //     // name: 'password',
+            // },
+            // {
+            //     // type: 'password',
+            //     // message: 'Re-enter password to confirm:',
+            //     // name: 'confirm',
+            // },
+        ])
+
+        .then((response) => {
+
+            switch (response.options) {
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "view Employees":
+                    viewEmployees();
+                    break;
+                case "View Departments":
+                    viewDepartments();
+                    break;
+                case "View Roles":
+                    viewRoles();
+                    break;
+
+            }
+
+            // if (response.options === "Add Employee") {
+            //     addEmployee();
+
+            // } else if (response.options === "Add Department") {
+            //     addDepartment();
+
+            // } else if (response.options === " Add Role") {
+            //     addRole();
+
+            // } else if (response.options === "View Employees") {
+            //     viewEmployees();
+
+            // } else if (response.options === "View Departments") {
+            //     viewDepartments()
+
+
+            // };
+        })
+}
+
+//----------------------------------------------------------------------------------------------------------------------------
 
 function addEmployee() {
 
@@ -59,13 +114,8 @@ function addEmployee() {
             },
             {
                 type: 'input',
-                message: 'Please add a department ID',
-                name: 'department_id'
-            },
-            {
-                type: 'input',
                 message: 'Please add a role ID',
-                name: 'roll_id'
+                name: 'role_id'
             }, {
                 type: 'input',
                 message: 'Please add a manager ID',
@@ -75,12 +125,106 @@ function addEmployee() {
         ])
 
         .then((response) => {
+            const query = connection.query(
+                'INSERT INTO EMPLOYEE SET ?', {
+
+                first_name: response.first_name,
+                last_name: response.last_name,
+                role_id: response.role_id,
+                manager_id: response.manager_id,
+
+
+            },
+                (err, res) => {
+                    if (err) throw err;
+                    console.log(`\nEmployee Added!\n`);
+                    mainMenu();
+                }
+
+            );
 
             console.log(response);
         });
 }
 
+//----------------------------------------------------------------------------------------------------------------------------
+
 function addDepartment() {
-    console.log("user selected Add department");
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Please input a department name ',
+                name: 'department_name',
+            },
+
+        ])
+
+        .then((response) => {
+            const query = connection.query(
+                'INSERT INTO DEPARTMENT SET ?', {
+
+                name: response.department_name,
+
+            },
+                (err, res) => {
+                    if (err) throw err;
+                    console.log(`\nDepartment Added!\n`);
+                    mainMenu();
+                }
+
+            );
+
+            console.log(response);
+        });
 }
 
+//----------------------------------------------------------------------------------------------------------------------------
+
+function addRole() {
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Please input a role name',
+                name: 'department_name',
+            },
+            {
+                type: 'input',
+                message: 'Please input a department name ',
+                name: 'department_name',
+            },
+            {
+                type: 'input',
+                message: 'Please input a department name ',
+                name: 'department_name',
+            },
+
+        ])
+
+        .then((response) => {
+            const query = connection.query(
+                'INSERT INTO DEPARTMENT SET ?', {
+
+                name: response.department_name,
+
+            },
+                (err, res) => {
+                    if (err) throw err;
+                    console.log(`\nDepartment Added!\n`);
+                    mainMenu();
+                }
+
+            );
+
+            console.log(response);
+        });
+}
+
+//----------------------------------------------------------------------------------------------------------------------------
+
+function viewEmployees() {
+
+}
